96dfa7838138f6317873bf1276ded8c205768e27
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-02 22:09:48 -0700

    Update MainActivity.kt

--- android/app/src/main/java/com/canc/starfox64/MainActivity.kt
+++ android/app/src/main/java/com/canc/starfox64/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.canc.starfox64
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import org.libsdl.app.SDLActivity
 
 class MainActivity : SDLActivity() {
-
     companion object {
         init {
-            System.loadLibrary("Starfox64Recompiled")
+            // Load the native library
+            System.loadLibrary("starfox64recompiled")
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun getLibraries(): Array<String> {
+        return arrayOf("starfox64recompiled")
     }
 }
